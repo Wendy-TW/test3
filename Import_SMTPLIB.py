dd1a90c63d4b0b053bc062aa30658a214758980d
wendy <wendy@email.com>
2024-03-23 14:27:51 +0800

    v3

---
+++ Import_SMTPLIB.py
@@ -0,0 +1,6 @@
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+
---
+++ email_20240323.py
@@ -0,0 +1,6 @@
+import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+print("hello")
